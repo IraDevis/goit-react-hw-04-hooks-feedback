ff03617e1ac44870292a11a0b513ca3557ec063c
Iryna Volosozharova <iradevis@gmail.com>
2021-07-31 22:19:41 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import Section from "./components/Section";
 import FeedbackOptions from "./components/FeedbackOptions";
 import Statistics from "./components/Statistics";
